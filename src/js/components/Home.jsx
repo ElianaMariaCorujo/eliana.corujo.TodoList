0dd357e26f1ded961345ba6b848d7258afd5f198
Eliana Corujo <elianamariacorujo@gmail.com>
2025-10-20 18:51:37 +0000

    Cambios

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
+
+function TodoList() {
+    const [tasks, setTasks] = useState([]); 
+    const [inputValue, setInputValue] = useState(""); 
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter' && inputValue.trim() !== "") {
+            setTasks([...tasks, inputValue]); 
             
+            setInputValue(""); 
+        }
+    };
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+    const deleteTask = (indexToDelete) => {
 
-export default Home;
+        const newTasks = tasks.filter((task, index) => index !== indexToDelete);
+        
+        setTasks(newTasks);
+    };
+
+    return (
+        <div className="todo-container">
+            <h1>todos</h1>
+            <div className="todo-list-paper">
+                <input 
+                    type="text" 
+                    placeholder="What needs to be done?"
+                    value={inputValue} 
+                    onChange={(e) => setInputValue(e.target.value)} 
+                    onKeyDown={handleKeyDown} 
+                />
+                <ul>
+                    {/* 4. RENDERIZADO CONDICIONAL Y DE LISTAS */}
+                    {tasks.length === 0 ? (
+                        <li className="no-tasks">No hay tareas, añadir tareas</li>
+                    ) : (
+                        tasks.map((task, index) => (
+                            <li key={index}>
+                                {task}
+                                {/* El ícono de eliminar llama a deleteTask con su 'index' */}
+                                <span 
+                                    className="delete-icon" 
+                                    onClick={() => deleteTask(index)}
+                                >
+                                    X
+                                </span>
+                            </li>
+                        ))
+                    )}
+                </ul>
+                <div className="footer">
+                    {/* 5. CONTADOR DE TAREAS */}
+                    {tasks.length} item left
+                </div>
+            </div>
+            {/* Estos divs extra son para el efecto de "stack" de papel */}
+            <div className="paper-stack-2"></div>
+            <div className="paper-stack-3"></div>
+        </div>
+    );
+}
+
+export default TodoList;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client'
 
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
-
-// index.css'
-import '../styles/index.css'
+import "bootstrap";
+import '../styles/index.css';
 
 // components
-import Home from './components/Home';
+import Home from './components/Home.jsx' ;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
